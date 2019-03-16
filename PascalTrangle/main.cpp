b84b6765221ce73acc003199a827c64d66b159d3
吴应金 <wu_yj@piec.com.cn>
2019-03-16 13:47:26 +0800

    demo

--- PascalTrangle/main.cpp
+++ PascalTrangle/main.cpp
@@ -22,6 +22,38 @@ public:
         }
         return res;
     }
+
+//    vector<int> getRow(int rowIndex) {
+//        vector<vector<int>> res;
+//        if (rowIndex < 1) return {};
+//        vector<int> thRows = {1};
+//        res.push_back(thRows);
+//
+//        for (int i = 1; i < rowIndex; ++i) {
+//            thRows.clear();
+//            thRows.push_back(1);
+//            for (int j = 1; j < i; ++j) {
+//                thRows.push_back(res[i - 1][j-1] + res[i - 1][j]);
+//            }
+//            thRows.push_back(1);
+//            res.push_back(thRows);
+//        }
+//        return res[rowIndex-1];
+//    }
+
+    vector<int> getRow(int rowIndex) {
+        vector<int> vi(rowIndex + 1);
+        vi[0] = 1;
+        for (int i = 0; i <= rowIndex; ++i) {
+            for (int j = i; j > 0; --j) {
+                vi[j] = vi[j] + vi[j-1];
+            }
+        }
+        return vi;
+    }
+
+
+
 };
 
 void printRes(vector<vector<int>> &res) {
